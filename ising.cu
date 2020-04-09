f8fe77c32d85c0c20a7a73e4035713b2b81bd98d
dquigley-warwick <d.quigley@warwick.ac.uk>
2020-04-09 12:20:54 +0100

    Tidied up main

--- ising.cu
+++ ising.cu
@@ -15,113 +15,135 @@ for gathering rare event statistics on nucleation during magnetisation reversal.
 #include <string.h>
 #include <time.h>  
 #include <float.h>
+#include <stdbool.h>
 
 extern "C" {
   #include "mc_cpu.h"
 }
 
 #include "mc_gpu.h"
-//#include "gpu_tools.h"
+#include "gpu_tools.h"
+
+const bool run_gpu = true;  // Run using GPU
+const bool run_cpu = true;  // Run using CPU
 
 int main (int argc, char *argv[]) {
 
+/*=================================
+   Defaults and constants 
+  =================================*/ 
+  
+  int L       = 64;   // Size of 2D Ising grid. LxL grid squares.
+  int ngrids  = 1;    // Number of replicas of 2D grid to simulate
+  int nsweeps = 100;  // Number of MC sweeps to simulate on each grid
 
+  double beta = 1.0/1.5;  // Inverse temperature
+  double h = 0.05;        // External field
+ 
+  unsigned long rngseed = 2894203475;  // RNG seed (fixed for development/testing)
+  
+  int threadsPerBlock = 1;  // Number of threads/replicas to run in each threadBlock
+  int blocksPerGrid   = 1;  // Total number of threadBlocks
+
+/*=================================
+   Process command line arguments 
+  =================================*/ 
   if (argc != 4) {
     printf("Usage : ./ising2D nsweeps nreplicas GPUthreadsPerBlock\n");
     exit(EXIT_FAILURE);
   }
 
-  gpuInit();
-
-  // Variables to hold the dimensions of the block
-  // and thread grids. 
-  int blocksPerGrid,threadsPerBlock;
-
-  int i,igrid;
-
-  char *a = argv[2];
-  int ngrids = atoi(a); // Number of grids to simulate
-  int L = 64;           // Size of L x L 2D grid
-
-  //threadsPerBlock = 1 ; // Max if using single thread with 32 bits per spin at L=64
-
-  a = argv[3];
-  threadsPerBlock = atoi(a);
-
-  blocksPerGrid = ngrids/threadsPerBlock;
-  if (ngrids%threadsPerBlock!=0) { blocksPerGrid += 1; }
-
-  fprintf(stderr, "threadsPerBlock = %d, blocksPerGrid = %d\n",threadsPerBlock, blocksPerGrid);
-
-  // Host memory
+  nsweeps = atoi(argv[1]);         // Number of MC sweeps to simulate
+  ngrids  = atoi(argv[2]);         // Number of replicas (grids) to simulate
+  threadsPerBlock = atoi(argv[3]); // Number of threads per GPU threadblock
+  
+/*=================================
+   Initialise simulations
+  =================================*/ 
+  // Host copy of Ising grid configurations
   int *ising_grids = (int *)malloc(L*L*ngrids*sizeof(int));
-
-  // Device memory
-  int *d_ising_grids;
-  gpuErrchk(cudaMalloc(&d_ising_grids,L*L*ngrids*sizeof(int)));
-
+  
   // Initialise as spin down
+  int i;
   for (i=0;i<L*L*ngrids;i++) { ising_grids[i] = -1; }
 
-  // Copy to devicef
-  gpuErrchk(cudaMemcpy(d_ising_grids,ising_grids,L*L*ngrids*sizeof(int),cudaMemcpyHostToDevice));
-  
-  // RNG 
-  unsigned long rngseed = 2894203475;
-
-  // - CPU
+  // Initialise host RNG
   init_genrand(rngseed);
 
-  // - GPU
-  curandStatePhilox4_32_10_t *d_state;
-  gpuErrchk (cudaMalloc((void **)&d_state, ngrids*sizeof(curandStatePhilox4_32_10_t)) );
-  unsigned long long gpuseed = (unsigned long long)rngseed;
-  init_gpurand<<<blocksPerGrid,threadsPerBlock>>>(gpuseed, d_state);
-  gpuErrchk( cudaPeekAtLastError() );
-  gpuErrchk( cudaDeviceSynchronize() );
+  int *d_ising_grids;                    // Pointer to device grid configurations
+  curandStatePhilox4_32_10_t *d_state;   // Pointer to device RNG states
 
-
-  double beta = 1.0/1.5;
-  double h = 0.05;
-
-
-  
-
-
-  int isweep;
-  a = argv[1];
-  int nsweeps = atoi(a);
-
-  clock_t t1 = clock();
-
-  for (isweep=0;isweep<nsweeps;isweep++){
-
-    // MC Sweep - CPU
-    for (igrid=0;igrid<ngrids;igrid++) {
-      mc_sweep_cpu(L,ising_grids,igrid,1.0/1.5,0.05);
-    }
+  if (run_gpu==true) {
     
-  }
+    gpuInit(); // Initialise GPU device(s)
 
-  clock_t t2 = clock();
+    // Device copy of Ising grid configurations
+    gpuErrchk( cudaMalloc(&d_ising_grids,L*L*ngrids*sizeof(int)) );
 
-  printf("Time taken on CPU = %f seconds\n",(double)(t2-t1)/(double)CLOCKS_PER_SEC);
+    // Populate from host copy
+    gpuErrchk( cudaMemcpy(d_ising_grids,ising_grids,L*L*ngrids*sizeof(int),cudaMemcpyHostToDevice) );
 
-  t1 = clock();
-
-  for (isweep=0;isweep<nsweeps;isweep++){
-
-    // MC Sweep - GPU
-    mc_sweep_gpu<<<blocksPerGrid,threadsPerBlock>>>(L,d_state,ngrids,d_ising_grids,(float)beta,(float)h);
+    // Initialise GPU RNG
+    gpuErrchk (cudaMalloc((void **)&d_state, ngrids*sizeof(curandStatePhilox4_32_10_t)) );
+    unsigned long long gpuseed = (unsigned long long)rngseed;
+    init_gpurand<<<blocksPerGrid,threadsPerBlock>>>(gpuseed, d_state);
     gpuErrchk( cudaPeekAtLastError() );
     gpuErrchk( cudaDeviceSynchronize() );
 
+    // Allocate threads to thread blocks
+    blocksPerGrid = ngrids/threadsPerBlock;
+    if (ngrids%threadsPerBlock!=0) { blocksPerGrid += 1; }
+  
+    fprintf(stderr, "threadsPerBlock = %d, blocksPerGrid = %d\n",threadsPerBlock, blocksPerGrid);
+
   }
 
-  t2 = clock();
+/*=================================
+    Run simulations
+  =================================*/ 
+  clock_t t1,t2;  // For measuring time taken
+  int isweep;     // MC sweep loop counter
 
-  printf("Time taken on GPU = %f seconds\n",(double)(t2-t1)/(double)CLOCKS_PER_SEC);
+  if (run_cpu==true) {
 
-  return 0;
+    int igrid;   // counter for loop over replicas
+
+    t1 = clock();
+
+    for (isweep=0;isweep<nsweeps;isweep++){
+
+      // MC Sweep - CPU
+      for (igrid=0;igrid<ngrids;igrid++) {
+        mc_sweep_cpu(L,ising_grids,igrid,1.0/1.5,0.05);
+      }
+    
+    }
+
+    t2 = clock();
+
+    printf("Time taken on CPU = %f seconds\n",(double)(t2-t1)/(double)CLOCKS_PER_SEC);
+
+  }
+
+  if (run_gpu==true){
+
+    t1 = clock();
+
+    for (isweep=0;isweep<nsweeps;isweep++){
+
+      // MC Sweep - GPU
+      mc_sweep_gpu<<<blocksPerGrid,threadsPerBlock>>>(L,d_state,ngrids,d_ising_grids,(float)beta,(float)h);
+      gpuErrchk( cudaPeekAtLastError() );
+      gpuErrchk( cudaDeviceSynchronize() );
+
+    }
+
+    t2 = clock();
+
+    printf("Time taken on GPU = %f seconds\n",(double)(t2-t1)/(double)CLOCKS_PER_SEC);
+
+  }
+
+  return EXIT_SUCCESS;
 
 }
