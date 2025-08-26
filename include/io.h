2ea54f44760a925d6c597903384157774844bced
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2025-08-26 14:18:33 +0100

    Created new HDF5 functions WIP

--- include/io.h
+++ include/io.h
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <hdf5.h>
 
 int write_ising_grids(int L, int ngrids, int *ising_grids, int isweep, float *magnetisation, float *lclus_size);
+int create_ising_grids_hdf5(int L, int ngrids, int tot_nsweeps, double h, double beta);
+int write_ising_grids_hdf5(int L, int ngrids, int *ising_grids, int isweep, float *magnetisation, float *lclus_size);
 void read_input_grid(int L, int ngrids, int *ising_grids);
