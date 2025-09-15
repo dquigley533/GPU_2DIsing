3dae9cd80af89c2030bb7bfc6af2c71d5ed9e24a
dquigley-warwick <d.quigley@warwick.ac.uk>
2025-09-15 16:22:09 +0100

    Driver routines now use updated function prototype for outfunc

--- src/mc_gpu.cu
+++ src/mc_gpu.cu
@@ -699,7 +699,7 @@ void mc_driver_gpu(mc_gpu_grids_t grids, double beta, double h, int* grid_fate, 
       
       // Writing of the grids can be happening on the host while the device runs the mc_sweep kernel
       if (isweep%grid_output_int==0 || isweep==tot_nsweeps-1){
-        outfunc(L, ngrids, ising_grids, isweep, magnetisation, lclus_size);  
+        outfunc(L, ngrids, ising_grids, isweep, magnetisation, lclus_size, cv, up_thr, dn_thr);  
       }
 
       // Write and report cv - can also be happening while the device runs the mc_sweep kernel
